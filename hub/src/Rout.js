a92bbfaae86ddb1c086b4ddb8755d8a384fb9cd6
JOYSTON <116254639+Joystonm@users.noreply.github.com>
2023-10-29 11:57:47 +0530

    navbar

--- hub/src/Rout.js
+++ hub/src/Rout.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Home from './Home'
+import Signin from './component/Signin'
+import Signup from './component/Signup'
+// import NoNavFooterLayout from './NoNavFooterLayout';
+
+
+
+
+
 const Rout = () => {
   return (
     <>
         <Routes>
             <Route path='/' element={<Home/>} />
+
+            <Route path='/sign-in' element={ <NoNavFooterLayout><Signin/></NoNavFooterLayout>}/>
+            <Route path='/sign-up' element={<Signup/>}/>
         </Routes>
     </>
   )
--- hub/src/component/Nav.js
+++ hub/src/component/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../style/nav.css'
 import {BsBagCheck} from 'react-icons/bs';
+import {Link} from 'react-router-dom'
 const Nav = () => {
   return (
     <>
@@ -8,17 +9,41 @@ const Nav = () => {
         <div className='container'>
             <div className='logo'>
                 <img src='./images/OIG_mcR5i1qxke-removebg-preview-transformed.png' alt='logo' ></img>
+                <p className='logo-header'>DEALHUB</p>
             </div>
 
             <div className='search-box'>
                 <input type='text' placeholder='Search'></input>
                 <button>Search</button>
-                <BsBagCheck/>
             </div>
 
-           
+            <div className='icon'>
+                 <BsBagCheck/>
+            </div>
 
-            
+            <div className='header'>
+                <div className='container'>
+                    <div className='nav'>
+                        <ul>
+                            <li>
+                                <Link to='/' className='link'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/product' className='link'>Product</Link>
+                            </li>
+                            <li>
+                                <Link to='sell' className='link'>Sell</Link>
+                            </li>
+                            <li>
+                                <Link to='sign-in' className='link'>Sign-in</Link>
+                            </li>
+                            <li>
+                                <Link to='sign-up' className='link'>Sign-up</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>       
         </div>
     </div>
     </>
